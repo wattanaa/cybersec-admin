0dbcdfa0544d5c3d27be498d0aae44e29108cab7
wattanaa <wattananatanjai2545@gmail.com>
2025-02-25 14:58:12 +0700

    update

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-const express = require("express");
+const { Prisma } = require('@prisma/client');
+const express = require('express');
+const {PrismaClient} = require('@prisma/client');
 
 const app = express();
+const prisma = new PrismaClient();
 
+const port = 3000
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/user',async(req, res) => {
+    const data = await prisma.user.findMany();
+    res.json({
+        message: 'ok',
+        data
+    });
+});
+
+app.post('/user',async(req, res) => {
+    console.log(req);
+    const response = await prisma.user.create(req.body);
+    res.json({
+        message: 'add data sucessfuly'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 });
